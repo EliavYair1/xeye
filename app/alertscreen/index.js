b164852998e6f044e99456c50a6a0f3e73d39709
Eliav Yair <eliav@moburst.com>
2023-11-22 17:45:48 +0200

    navbar status update .

--- app/alertscreen/index.js
+++ app/alertscreen/index.js
@@ -73,7 +73,7 @@ const AlertScreen = () => {
       />
       <View style={styles.imageWrapper}>
         <TargetThreat
-          imageHeight={windowHeight >= 844 ? 626 : 600}
+          imageHeight={windowHeight - 200}
           imageWidth={windowWidth - 32}
           gunBgWidth={229}
           gunBgHeight={567}
