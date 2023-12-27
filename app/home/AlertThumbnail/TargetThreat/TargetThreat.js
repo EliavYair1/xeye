68a97633cd5ece0ebc4d7ddf59f26b4e084e98fc
nirlewinsohn <nirlewin@gmail.com>
2023-12-27 11:34:56 +0200

    fix alert image

--- app/home/AlertThumbnail/TargetThreat/TargetThreat.js
+++ app/home/AlertThumbnail/TargetThreat/TargetThreat.js
@@ -1,24 +1,49 @@
-import React from "react";
-import { View, ImageBackground } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Image } from "react-native";
 
-const TargetThreat = ({ imageWidth, imageHeight, alert }) => {
+const TargetThreat = React.memo(({ imageHeight, imageWidth, alert }) => {
+  // console.log("props", props);
+  // const alert = props.alert;
+  const [imageStyle, setImageStyle] = useState({
+    // width: 100,
+    // height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  });
+  useEffect(() => {
+    if (alert) {
+      Image.getSize(alert.snapshot, (width, height) => {
+        console.log("width, height", width, height);
+        if (width > height) {
+          setImageStyle((prev) => {
+            return {
+              ...prev,
+              width: width > imageWidth ? imageWidth : width,
+              aspectRatio: width / height,
+            };
+          });
+        } else {
+          setImageStyle((prev) => {
+            return {
+              ...prev,
+              height: height,
+              aspectRatio: width / height,
+              maxHeight: imageHeight,
+            };
+          });
+        }
+      });
+    }
+  }, [alert]);
+
   if (!alert || !alert.snapshot) {
     return <View></View>;
   }
   return (
     <View style={{ marginBottom: 24 }}>
-      <ImageBackground
-        source={{ uri: alert.snapshot }}
-        style={{
-          width: imageWidth,
-          height: imageHeight,
-          resizeMode: "cover",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      ></ImageBackground>
+      <Image source={{ uri: alert.snapshot }} style={imageStyle}></Image>
     </View>
   );
-};
+});
 
 export default TargetThreat;
